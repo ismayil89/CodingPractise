5f0175ad243c7d87de50a35f3746a6ce0cc2b755
ismayil89 <67112141+ismayil89@users.noreply.github.com>
2020-12-11 00:37:05 +0530

    Create Reverse_LinkedList.py

---
+++ DSA/Python/Reverse_LinkedList.py
@@ -0,0 +1,32 @@
+'''
+LC #206 
+Reverse Linked List
+
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+'''
+# Iterative Method
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        Ptr_previous = None
+        Ptr_current = head
+        while Ptr_current:
+            Ptr_next = Ptr_current.next
+            Ptr_current.next = Ptr_previous
+            Ptr_previous = Ptr_current
+            Ptr_current = Ptr_next
+            
+        return Ptr_previous
