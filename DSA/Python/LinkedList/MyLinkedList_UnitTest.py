f97cf0f3e190657a5cdf4645fe05db8f265b4d42
ismayil89 <67112141+ismayil89@users.noreply.github.com>
2020-12-15 23:06:49 +0530

    Create MyLinkedList_UnitTest.py
    Unit Tests for Linked List

---
+++ DSA/Python/LinkedList/MyLinkedList_UnitTest.py
@@ -0,0 +1,15 @@
+import unittest
+import MyLinkedList as MyLL
+class Test_DynamicArray(unittest.TestCase):
+    def setUp(self):
+        self.testObj = MyLL.MyLinkedList()
+        self.myListElements = [5,15,25]
+        self.mylistLen = len(self.myListElements)
+        for element in self.myListElements:
+            self.testObj.addAtHead(element)
+    
+    def test_get(self):
+        self.assertEqual(25, self.testObj.get(0))
+
+if __name__ == '__main__':
+    unittest.main()
